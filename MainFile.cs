0f3eb756a4b0b920b5d9fc808104291423638a94
Alchyr <isithalchyr@gmail.com>
2026-03-06 22:55:34 -0800

    Change ModId to private

--- MainFile.cs
+++ MainFile.cs
@@ -7,7 +7,7 @@ namespace ModTemplate;
 [ModInitializer(nameof(Initialize))]
 public partial class MainFile : Node
 {
-    internal const string ModId = "ModTemplate";
+    private const string ModId = "ModTemplate"; //At the moment, this is used only for the Logger and harmony names.
 
     public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } = new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
 
